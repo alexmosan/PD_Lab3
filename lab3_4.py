9a918d92849c141ce86193368a8131058c8eb7f3
Alexandr M <a.mosan@econe.md>
2023-10-23 10:13:00 +0300

    Fix: Fix to get unique instances

--- lab3_4.py
+++ lab3_4.py
@@ -1,29 +1,30 @@
 import os
 import random
 
-def get_instances_of_class(class_label, dataset_path):
-    instances = []
-    for root, dirs, files in os.walk(dataset_path):
-        if os.path.basename(root) == class_label:
-            instances.extend([os.path.join(root, filename) for filename in files])
-    random.shuffle(instances)
-    return instances
-
 def get_next_instance(class_label, dataset_path):
-    instances = get_instances_of_class(class_label, dataset_path)
-    for instance in instances:
-        yield instance
+    if not hasattr(get_next_instance, "instances"):
+        instances = []
+
+        for root, dirs, files in os.walk(dataset_path):
+            if os.path.basename(root) == class_label:
+                instances.extend([os.path.join(root, filename) for filename in files])
+
+        random.shuffle(instances)
+
+        get_next_instance.instances = instances
+
+    if get_next_instance.instances:
+        return get_next_instance.instances.pop()
+    else:
+        return None
 
 class_label = 'good'
 dataset_path = 'D:/3-rd course/PD/PD_Lab3/dataset'
 
-next_instance_generator = get_next_instance(class_label, dataset_path)
+for _ in range(10):
+    next_instance = get_next_instance(class_label, dataset_path)
 
-for next_instance in next_instance_generator:
-    print(f"Следующий экземпляр класса {class_label}: {next_instance}")
-
-class_label = 'bad'
-next_instance_generator = get_next_instance(class_label, dataset_path)
-
-for next_instance in next_instance_generator:
-    print(f"Следующий экземпляр класса {class_label}: {next_instance}")
+    if next_instance:
+        print(f"Следующий экземпляр класса {class_label}: {next_instance}")
+    else:
+        print(f"Все экземпляры класса {class_label} закончились.")
